088c56fc8cde88c667a23064fcf0b45948990056
PETER ROBINSON <peter.robinson@cnh.com>
2025-11-28 10:07:05 +0000

    BlockTrailer - Support ANSI PSTs

--- PSTParse/NDB/BlockTrailer.cs
+++ PSTParse/NDB/BlockTrailer.cs
@@ -13,8 +13,19 @@ namespace PSTParse.NDB
         {
             this.DataSize = BitConverter.ToUInt16(bytes, offset);
             this.WSig = BitConverter.ToUInt16(bytes, 2 + offset);
-            this.CRC = BitConverter.ToUInt32(bytes, 4 + offset);
-            this.BID_raw = BitConverter.ToUInt64(bytes, 8 + offset);
+
+            if (bytes.Length >= 16 + offset)
+            {
+                // Unicode Block Trailer layout has 4-byte CRC, followed by 8-byte BID
+                this.CRC = BitConverter.ToUInt32(bytes, 4 + offset);
+                this.BID_raw = BitConverter.ToUInt64(bytes, 8 + offset);
+            }
+            else
+            {
+                // ANSI Block Trailer has a 4-byte BID, followed by a 4-byte CRC
+                this.BID_raw = BitConverter.ToUInt32(bytes, 4 + offset);
+                this.CRC = BitConverter.ToUInt32(bytes, 8 + offset);
+            }
         }
     }
 }
