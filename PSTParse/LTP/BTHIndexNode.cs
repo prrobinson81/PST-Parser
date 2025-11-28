0cb5c689d5ff230c1236afc657be4f47431b4239
PETER ROBINSON <peter.robinson@cnh.com>
2025-11-28 09:39:50 +0000

    BTHIndexNode - simplified formatting, removed redundant code.

--- PSTParse/LTP/BTHIndexNode.cs
+++ PSTParse/LTP/BTHIndexNode.cs
@@ -20,42 +20,32 @@ namespace PSTParse.LTP
             this.Level = level;
             this.HID = hid;
             if (hid.hidBlockIndex == 0 && hid.hidIndex == 0)
+            {
                 return;
-            
+            }
+
             this.Entries = new List<BTHIndexEntry>();
 
             if (level == 0)
             {
                 this.Data = new BTHDataNode(hid, tree);
-                /*
-                for (int i = 0; i < bytes.Length; i += (int)tree.Header.KeySize + 4)
-                    this.Entries.Add(new BTHIndexEntry(bytes, i, tree.Header));
-                this.DataChildren = new List<BTHDataNode>();
-                foreach(var entry in this.Entries)
-                    this.DataChildren.Add(new BTHDataNode(entry.HID, tree));*/
-            } else
+            }
+            else
             {
                 var bytes = tree.GetHIDBytes(hid);
+
                 for (int i = 0; i < bytes.Data.Length; i += (int)tree.Header.KeySize + 4)
+                {
                     this.Entries.Add(new BTHIndexEntry(bytes.Data, i, tree.Header));
+                }
+
                 this.Children = new List<BTHIndexNode>();
-                foreach(var entry in this.Entries)
+
+                foreach (var entry in this.Entries)
+                {
                     this.Children.Add(new BTHIndexNode(entry.HID, tree, level - 1));
+                }
             }
-
-        }
-
-        public bool BlankPassword(PSTFile pst)
-        {
-            if (this.Data != null)
-                return this.Data.BlankPassword(pst);
-
-            foreach (var child in Children)
-                child.BlankPassword(pst);
-                /*if (child.BlankPassword(Data) != null)
-                    return child.BlankPassword(Data);*/
-
-            return false;
         }
     }
 }
