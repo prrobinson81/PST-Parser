19a3cb72f12ebe2250a78f1ec19a88a4b880e6c4
PETER ROBINSON <peter.robinson@cnh.com>
2025-11-28 08:56:54 +0000

    Updated PSTParseApp to automatically loop through 4 test files covering combinations of ANSI, Unicode, password, and no password.

--- PSTParseApp/Program.cs
+++ PSTParseApp/Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using PSTParse;
+using PSTParse.Message_Layer;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
-using PSTParse;
-using PSTParse.Message_Layer;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PSTParseApp
 {
@@ -15,65 +16,98 @@ namespace PSTParseApp
         {
             var sw = new Stopwatch();
             sw.Start();
-            var pstPath = @"C:\test\dtmtcm@gmail.com.pst";
-            var logPath = @"C:\test\nidlog.txt";
+
+            List<string> pstPaths = new List<string>()
+            {
+                @"C:\Users\nt084\Outlook\Test PST - no pwd - ANSI.pst",
+                @"C:\Users\nt084\Outlook\Test PST - no pwd.pst",
+                @"C:\Users\nt084\Outlook\Test PST - pwd - ANSI.pst",
+                @"C:\Users\nt084\Outlook\Test PST - pwd.pst",
+            };
+
+            foreach (var pstPath in pstPaths)
+            {
+                ProcessPST(pstPath);
+                Console.WriteLine();
+            }
+
+            sw.Stop();
+            Console.WriteLine("Total Time: {0} milliseconds", sw.ElapsedMilliseconds);
+        }
+
+        static void ProcessPST(string pstPath)
+        { 
+            Console.WriteLine("Opening PST: " + pstPath);
+
+            // var logPath = @"C:\Users\nt084\Downloads\vp346.txt"; // "C:\test\nidlog.txt";
             var pstSize = new FileInfo(pstPath).Length*1.0/1024/1024;
+            Console.WriteLine("PST Size: {0:0.00} MB", pstSize);
+
             using (var file = new PSTFile(pstPath))
             {
-                Console.WriteLine("Magic value: " + file.Header.DWMagic);
-                Console.WriteLine("Is Ansi? " + file.Header.IsANSI);
+                //Console.WriteLine("Magic value: " + file.Header.DWMagic);
+                Console.WriteLine("Is Ansi: " + file.Header.IsANSI);
 
+                //var lookup = new PSTParse.Message_Layer.NamedToPropertyLookup(file);
 
-                var stack = new Stack<MailFolder>();
-                stack.Push(file.TopOfPST);
-                var totalCount = 0;
-                if (File.Exists(logPath))
-                    File.Delete(logPath);
-                using (var writer = new StreamWriter(logPath))
-                {
-                    while (stack.Count > 0)
-                    {
-                        var curFolder = stack.Pop();
+                // Write all Properties out from the dictionary with their name and value
+                //foreach (var prop in lookup.PC.Properties)
+                //{
+                //    Console.WriteLine("Property: {0} - {1}", prop.Key, prop.Value);
+                //}
 
-                        foreach (var child in curFolder.SubFolders)
-                            stack.Push(child);
-                        var count = curFolder.ContentsTC.RowIndexBTH.Properties.Count;
-                        totalCount += count;
-                        Console.WriteLine(String.Join(" -> ", curFolder.Path) + " ({0} messages)", count);
+                Console.WriteLine("Is Password Protected: " + file.IsPasswordProtected());
+
+                //var stack = new Stack<MailFolder>();
+                //stack.Push(file.TopOfPST);
+                //var totalCount = 0;
+                //if (File.Exists(logPath))
+                //    File.Delete(logPath);
+                //using (var writer = new StreamWriter(logPath))
+                //{
+                //    while (stack.Count > 0)
+                //    {
+                //        var curFolder = stack.Pop();
+
+                //        foreach (var child in curFolder.SubFolders)
+                //            stack.Push(child);
+                //        var count = curFolder.ContentsTC.RowIndexBTH.Properties.Count;
+                //        totalCount += count;
+                //        Console.WriteLine(String.Join(" -> ", curFolder.Path) + " ({0} messages)", count);
                     
-                        foreach (var ipmItem in curFolder)
-                        {
-                            if (ipmItem is Message)
-                            {
-                                var message = ipmItem as Message;
-                                Console.WriteLine(message.Subject);
-                                Console.WriteLine(message.Imporance);
-                                Console.WriteLine("Sender Name: " + message.SenderName);
-                                if (message.From.Count > 0)
-                                    Console.WriteLine("From: {0}",
-                                                      String.Join("; ", message.From.Select(r => r.EmailAddress)));
-                                if (message.To.Count > 0)
-                                    Console.WriteLine("To: {0}",
-                                                      String.Join("; ", message.To.Select(r => r.EmailAddress)));
-                                if (message.CC.Count > 0)
-                                    Console.WriteLine("CC: {0}",
-                                                      String.Join("; ", message.CC.Select(r => r.EmailAddress)));
-                                if (message.BCC.Count > 0)
-                                    Console.WriteLine("BCC: {0}",
-                                                      String.Join("; ", message.BCC.Select(r => r.EmailAddress)));
+                //        foreach (var ipmItem in curFolder)
+                //        {
+                //            if (ipmItem is Message)
+                //            {
+                //                var message = ipmItem as Message;
+                //                Console.WriteLine(message.Subject);
+                //                Console.WriteLine(message.Imporance);
+                //                Console.WriteLine("Sender Name: " + message.SenderName);
+                //                if (message.From.Count > 0)
+                //                    Console.WriteLine("From: {0}",
+                //                                      String.Join("; ", message.From.Select(r => r.EmailAddress)));
+                //                if (message.To.Count > 0)
+                //                    Console.WriteLine("To: {0}",
+                //                                      String.Join("; ", message.To.Select(r => r.EmailAddress)));
+                //                if (message.CC.Count > 0)
+                //                    Console.WriteLine("CC: {0}",
+                //                                      String.Join("; ", message.CC.Select(r => r.EmailAddress)));
+                //                if (message.BCC.Count > 0)
+                //                    Console.WriteLine("BCC: {0}",
+                //                                      String.Join("; ", message.BCC.Select(r => r.EmailAddress)));
                                 
 
-                                writer.WriteLine(ByteArrayToString(BitConverter.GetBytes(message.NID)));
-                            }
-                        }
-                    }
-                }
-                sw.Stop();
-                Console.WriteLine("{0} messages total", totalCount);
-                Console.WriteLine("Parsed {0} ({2:0.00} MB) in {1} milliseconds", Path.GetFileName(pstPath),
-                                  sw.ElapsedMilliseconds, pstSize);
-                //file.Header.NodeBT.Root.GetOffset(1);
-                Console.Read();
+                //                writer.WriteLine(ByteArrayToString(BitConverter.GetBytes(message.NID)));
+                //            }
+                //        }
+                //    }
+                //}
+                //sw.Stop();
+                //Console.WriteLine("{0} messages total", totalCount);
+                //Console.WriteLine("Parsed {0} ({2:0.00} MB) in {1} milliseconds", Path.GetFileName(pstPath),
+                //                  sw.ElapsedMilliseconds, pstSize);
+                ////file.Header.NodeBT.Root.GetOffset(1);
+                //Console.Read();
             }
         }
 
