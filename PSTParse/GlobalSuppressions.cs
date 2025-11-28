6c88c6e7aa1c63f0e95987552a491fc82297c0ae
PETER ROBINSON <peter.robinson@cnh.com>
2025-11-28 09:13:39 +0000

    Implement StyleCop Analyzers to enforce documentation.

---
+++ PSTParse/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿//------------------------------------------------------------------------
+// <copyright file="GlobalSuppressions.cs" company="CNH N.V.">
+// Copyright (c) CNH N.V.. All rights reserved.
+// </copyright>
+// <remarks>
+// This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to this project.
+// Project-level suppressions either have no target or are given a specific target and scoped to a namespace, type, member, etc.
+// </remarks>
+// <author>Peter Robinson (CNH2\nt084)</author>
+//------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1124:Do not use regions", Justification = "I like regions.")]
