df5a245429c51f100edc1a8af9d0cbd2cbb1c90e
PETER ROBINSON <peter.robinson@cnh.com>
2025-11-28 10:00:44 +0000

    BBTENTRY - Support ANSI PSTs and standardise formatting.

--- PSTParse/NDB/BBTENTRY.cs
+++ PSTParse/NDB/BBTENTRY.cs
@@ -2,12 +2,20 @@
 
 namespace PSTParse.NDB
 {
-    public class BBTENTRY : BTPAGEENTRY
+    public interface IBBTENTRY
     {
-        public BREF BREF;
-        public bool Internal;
-        public UInt16 BlockByteCount;
-        public UInt16 RefCount;
+        IBREF BREF { get; set; }
+        bool Internal { get; set; }
+        UInt16 BlockByteCount { get; set; }
+        UInt16 RefCount { get; set; }
+    }
+
+    public class BBTENTRY : BTPAGEENTRY, IBBTENTRY
+    {
+        public IBREF BREF { get; set; }
+        public bool Internal { get; set; }
+        public UInt16 BlockByteCount { get; set; }
+        public UInt16 RefCount { get; set; }
 
         public BBTENTRY(byte[] bytes)
         {
@@ -17,6 +25,29 @@ namespace PSTParse.NDB
             this.Internal = this.BREF.IsInternal;
             this.BlockByteCount = BitConverter.ToUInt16(bytes, 16);
             this.RefCount = BitConverter.ToUInt16(bytes, 18);
+        }
+
+        public ulong Key
+        {
+            get { return BREF.BID; }
+        }
+    }
+
+    public class BBTENTRY_a : BTPAGEENTRY, IBBTENTRY
+    {
+        public IBREF BREF { get; set; }
+        public bool Internal { get; set; }
+        public UInt16 BlockByteCount { get; set; }
+        public UInt16 RefCount { get; set; }
+
+        public BBTENTRY_a(byte[] bytes)
+        {
+            this.BREF = new BREF_a(bytes);
+            /*this.BREF = new BREF_UNICODE
+                            {BID_raw = BitConverter.ToUInt64(bytes, 0), ByteIndex = BitConverter.ToUInt64(bytes, 8)};*/
+            this.Internal = this.BREF.IsInternal;
+            this.BlockByteCount = BitConverter.ToUInt16(bytes, 8);
+            this.RefCount = BitConverter.ToUInt16(bytes, 10);
         }
 
         public ulong Key
